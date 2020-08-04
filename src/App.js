36c19e98331af4e5143f2e956b96fe8138406aa6
mugishamatt <mugisham410@gmail.com>
2020-08-03 21:48:46 -0500

    added button and handle

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+//create class Component
+class ToggleCount extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      count:0,
+      // IsToggle:false
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     }
+  }
+  //handle event to update when user click buton
+  handleCount=()=>{
+    this.setState({
+      count:this.state.count +1,
+    }
+    )
+  }
+  render() { 
+    return (  
+      <div>
+            <button onClick ={this.handleCount}>
+               {this.state.count} 
+                      </button>
+      </div>
+    );
+  }
 }
-
-export default App;
+ 
+export default ToggleCount;
